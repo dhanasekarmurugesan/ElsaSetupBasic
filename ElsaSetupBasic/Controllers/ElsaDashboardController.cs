5b01e72aa57aeb2c936e3a442506c3cdf3f025cd
dhanasekar.muruges <dhanasekar.muruges@ASPLAP2159>
2023-03-02 16:35:03 +0530

    Add project files.

---
+++ ElsaSetupBasic/Controllers/ElsaDashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ElsaSetupBasic.Controllers
+{
+    public class ElsaDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Workflow";
+            return View();
+        }
+    }
+}
